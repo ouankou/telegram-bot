b46c4baa1a6003650aa5e1810435fc4712970ea5
Anjia Wang <anjiawang@gmail.com>
2020-07-26 15:31:18 -0400

    Go back to webhook since polling caused a lot of app crashes

--- bot.py
+++ bot.py
@@ -172,11 +172,10 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    #updater.start_webhook(listen="0.0.0.0",
-    #                      port=int(PORT),
-    #                      url_path=TOKEN)
-    #updater.bot.setWebhook(ADDRESS + TOKEN)
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook(ADDRESS + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
